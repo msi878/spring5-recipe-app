432bf5a60faf759dd15de363d19a8108a9ac68aa
Michael Finco <michael.finco@ptqs.it>
2019-09-06 12:25:28 +0200

    close 146

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO category (description) VALUES ('American');
+INSERT INTO category (description) VALUES ('Italian');
+INSERT INTO category (description) VALUES ('Mexican');
+INSERT INTO category (description) VALUES ('Fast Food');
+INSERT INTO unit_of_measure (description) VALUES ('Tea1');
+INSERT INTO unit_of_measure (description) VALUES ('Tea2');
+INSERT INTO unit_of_measure (description) VALUES ('Tea3');
+INSERT INTO unit_of_measure (description) VALUES ('Tea4');
+INSERT INTO unit_of_measure (description) VALUES ('Tea5');
